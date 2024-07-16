c0fe2da0ca3eae5237f4c25eac21403f63604c9e
David Karauni <davymwachits@gmail.com>
2024-07-16 12:36:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find() method to search for an object where result === "W"
+    const winningYearObj = record.find(game => game.result === "W");
+    
+    // Check if a winning year object was found
+    if (winningYearObj) {
+      return winningYearObj.year; // Return the year if found
+    } else {
+      return undefined; // Return undefined if no win is found
+    }
+  }
